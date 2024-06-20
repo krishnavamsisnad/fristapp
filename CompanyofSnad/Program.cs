c6408b19db8a5efc86c15644e2f5939732685ad5
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-06-20 19:35:41 +0530

    dd

--- CompanyofSnad/Program.cs
+++ CompanyofSnad/Program.cs
@@ -1,5 +1,8 @@
 
 using CompanyofSnad.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace CompanyofSnad
@@ -11,10 +14,28 @@ namespace CompanyofSnad
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+         
             builder.Services.AddDbContext<DataDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Companydb")));
+            builder.Services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+                options.ApiVersionReader= ApiVersionReader.Combine(
+                  new   QueryStringApiVersionReader("api-version"),
+                    new HeaderApiVersionReader("x-version"),
+                    new MediaTypeApiVersionReader("ver")
+                    );
+            
+            });
+            builder.Services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl= true;
+            });
 
             builder.Services.AddControllers();
+        
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
